c0e009e2b0db83dbfafb575e57c31da8aa8661ec
noiredev <noiresama@gmail.com>
2022-12-02 11:33:37 +0000

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import Header from "./components/header/Header"
+
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline text-purple-600">
-            Hello world!
-        </h1>
+        <Header />
     )
 }
