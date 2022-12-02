94519d685bf22318e844100b3618d3d77ff68c48
noiredev <noiresama@gmail.com>
2022-12-02 14:46:56 +0000

    Updated the fonts based on the feedback from the product owner

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,25 +4,25 @@
 const Footer = () => {
     return (
         <>
-        
-<footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 fixed bottom-0 w-full">
-    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://dfx.com/" className="hover:underline">DFX™</a>. All Rights Reserved.
-    </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
-        </li>
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" className="hover:underline">Contact</a>
-        </li>
-    </ul>
-</footer>
+
+            <footer className="font-sans p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 fixed bottom-0 w-full">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://dfx.com/" className="hover:underline">DFX™</a>. All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                    </li>
+                    <li>
+                        <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </footer>
 
         </>
     );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,9 @@
 const Header = () => {
     return (
         <>
-            <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+            <nav className="font-sans bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
                 <div className="container flex flex-wrap items-center justify-between mx-auto">
-                    <a href="" className="flex  items-center">
+                    <a href="/" className="flex  items-center">
                         {/* <img src="" className="h-6 mr-3 sm:h-9" alt="DFX Profile Logo" /> */}
                         <span className="text-blue-800 self-center text-3xl font-semibold whitespace-nowrap dark:text-white">DFX Profile</span>
                     </a>
