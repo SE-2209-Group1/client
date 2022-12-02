65566d63fda8ccc336a58fb932364582028a0dae
Ben Tracey <bptracey@btinternet.com>
2022-12-02 11:59:43 +0000

    footer added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Header from "./components/header/Header"
+import Footer from './components/header/Footer'
+
 
 export default function App() {
     return (
-        <Header />
-    )
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
 }
