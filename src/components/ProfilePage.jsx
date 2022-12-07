065b9bcfc3d07c2f2c575772aa2e9c4beef4d553
Mustafa-Habashi <mustafahabashi10@gmail.com>
2022-12-07 11:45:10 +0000

    created personal summary page

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import GradProfile from './graduates/GradProfile'
+import PersonalStorySummary from './graduates/PersonalStorySummary'
+import '../profilePage.css'
 
 const ProfilePage = ({ profiledata }) => {
     return (
         <>
-            <div >{profiledata.map(data =>
-                <GradProfile key={data.profileid}
-                    fullName={data.name}
-                    personalEmail={data.personalemail}
-                    workEmail={data.workemail}
-                    gender={data.gender}
-                    phoneNum={data.phoneno}
-                    nationality={data.nationality}
-                    personality={data.personality}
-                    githubLink={data.githublink}
-                    linkedinLink={data.linkedinlink}
+            <div className='profile'>{profiledata.map(data =>
+                <div className='grad'>
+                    <GradProfile key={data.profileid}
+                        fullName={data.name}
+                        personalEmail={data.personalemail}
+                        workEmail={data.workemail}
+                        gender={data.gender}
+                        phoneNum={data.phoneno}
+                        nationality={data.nationality}
+                        personality={data.personality}
+                        githubLink={data.githublink}
+                        linkedinLink={data.linkedinlink}
 
-                ></GradProfile>)}</div>
+                    ></GradProfile></div>
+
+            )}<div className='personal'><PersonalStorySummary></PersonalStorySummary></div></div>
 
         </>
     )
