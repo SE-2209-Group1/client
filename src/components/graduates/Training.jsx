ea0b7660b88b2c043dbcffa618f10f478e40a8ad
noire <41166930+noiredev@users.noreply.github.com>
2022-12-14 10:51:00 +0000

    Updated the training.jsx

--- src/components/graduates/Training.jsx
+++ src/components/graduates/Training.jsx
@@ -1,38 +1,30 @@
+import Card from "../Card";
+import Divider from "../Divider";
+
 const Training = ({ trainingData }) => {
 
     return (
         <>
-            <div className="my-4 flex flex-col 2xl:flex-row space-y-4 2xl:space-y-0 2xl:space-x-4">
-                {trainingData.map(data =>
-                    <div className="w-full flex flex-col 2xl:w-1/3">
-                        <div className="flex-1 bg-white rounded-lg shadow-xl p-8">
-                            <h4 className="text-xl text-gray-900 font-bold">Your Training</h4>
-                            <ul className="mt-2 text-gray-700">
-                                <li className="flex border-y py-2">
-                                    <span className="font-bold w-24">Cohort:</span>
-                                    <span className="text-gray-700">{data.cohort}</span>
-                                </li>
-                                <li className="flex border-b py-2">
-                                    <span className="font-bold w-24">Learning path:</span>
-                                    <span className="text-gray-700">{data.learningPath}</span>
-                                </li>
-                                <li className="flex border-b py-2">
-                                    <span className="font-bold w-24">Trainer:</span>
-                                    <span className="text-gray-700">{data.trainer}</span>
-                                </li>
-                                <li className="flex border-b py-2">
-                                    <span className="font-bold w-24">Training Finish Date:</span>
-                                    <span className="text-gray-700">{data.finishDate}</span>
-                                </li>
-                                <li className="flex border-b py-2">
-                                    <span className="font-bold w-24">Module Grades:</span>
-                                    <span className="text-gray-700">{data.moduleGrades}</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                )}
-            </div>
+            {trainingData.map(data =>
+                <Card key={data.profileid} header={"Your Training"}>
+                    <Divider>
+                        <dt className="text-sm font-medium text-gray-500">Cohort:</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.cohort}</dd>
+                    </Divider>
+                    <Divider gray={true}>
+                        <dt className="text-sm font-medium text-gray-500">Learning path:</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.learningPath}</dd>
+                    </Divider>
+                    <Divider>
+                        <dt className="text-sm font-medium text-gray-500">Trainer:</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.trainer}</dd>
+                    </Divider>
+                    <Divider gray={true}>
+                        <dt className="text-sm font-medium text-gray-500">Training Finish Date:</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{data.finishDate}</dd>
+                    </Divider>
+                </Card>
+            )}
         </>
     );
 };
